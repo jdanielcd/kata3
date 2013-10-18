97ba3599bcca44d5b2c9fdb35f1c645de79460fb
usuario <usuario@10.22.146.134>
2013-10-18 14:28:13 +0100

    Trabajo de clase (añadiendo el control)

--- src/kata3/main.java
+++ src/kata3/main.java
@@ -6,11 +6,7 @@ public class main {
 
 
     public static void main(String[] args) {
-        String filename = args[0];
-        HistogramBuilder<String> builder = new HistogramBuilder<>();
-        builder.calculate(MailReader.readDomains(filename));
-        HistogramViewer<String> viewer = new HistogramViewer(builder.getHistogram());
-        viewer.show();
-        
+        Control control = new Control(args[0]);
+        control.execute();        
     }
 }
